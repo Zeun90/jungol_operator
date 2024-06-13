8760f4a90c96e55c7c1b5c4676009b0b9dc92b4c
Lee JiEun <gsilver190@gmail.com>
2024-06-13 23:14:21 +0900

    자가 진단 6

---
+++ src/Q6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // 2개의 정수 입력
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        // 관계 연산자
+        System.out.printf("%d > %d --- %b\n", num1, num2, num1>num2);
+        System.out.printf("%d < %d --- %b\n", num1, num2, num1<num2);
+        System.out.printf("%d >= %d --- %b\n", num1, num2, num1>=num2);
+        System.out.printf("%d <= %d --- %b", num1, num2, num1<=num2);
+    }
+}
